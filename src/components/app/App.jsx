b4d5dc9ccfac4cc3e9bbb33692608b6df5eb967e
Emilix22 <torresdragon@hotmail.com>
2023-05-02 22:00:11 -0300

    organizo estructura de carpetas

---
+++ src/components/app/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="home">
+      sidebar
+      <div className="homeContainer">
+        navbar
+      </div>
+
+    </div>
+  )
+}
+
+export default App
