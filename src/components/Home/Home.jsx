c1a54c31d44f134b7a6dbdf74f04ff338c4b7fd2
Emilix22 <torresdragon@hotmail.com>
2023-05-13 19:19:56 -0300

    fetch a total de productos y usuarios

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import './Home.css'
 import Card from "../Card/Card";
 import GroupIcon from '@mui/icons-material/Group';
 import StoreIcon from '@mui/icons-material/Store';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
+
+
 function Home() {
+    const [totalUsers, setTotalUsers] = useState();
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/users')
+        .then(res => res.json())
+        .then(datos => {
+            setTotalUsers(datos.meta.total)
+        })
+    },[])
+
+    const [totalProducts, setTotalProducts] = useState();
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/products')
+        .then(res => res.json())
+        .then(datos => {
+            setTotalProducts(datos.meta.total)
+        })
+    },[])
+
     return (
         <div>
             <div className='cards'>
@@ -13,17 +35,17 @@ function Home() {
                     title="USUARIOS"
                     link="Ver todos los usuarios"
                     isMoney="#"
-                    counter="1254"
+                    counter={totalUsers}
                     icon= {<GroupIcon className="iconWidget" style={{color: "crimson", backgroundColor: "rgba(255, 0, 0, 0.2)"}} />}/>
 
                 <Card 
-                    title="USUARIOS"
-                    link="Ver todos los usuarios"
+                    title="PRODUCTOS"
+                    link="Ver todos los productos"
                     isMoney="#"
-                    counter="1254"
+                    counter={totalProducts}
                     icon={<StoreIcon className="iconWidget" style={{color: "goldenrod", backgroundColor: "rgba(218, 165, 32, 0.2)"}} />}/>
                 <Card 
-                    title="USUARIOS"
+                    title="GANANCIAS"
                     link="Ver todos los usuarios"
                     isMoney="$"
                     counter="1254"
